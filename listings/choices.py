fb0af26ff5f369e0f9b046d99a418ba8bd6908c5
Ruben Dias <rubenrgd@gmail.com>
2019-06-10 15:14:28 -0300

    Ate Seçao 8

---
+++ listings/choices.py
@@ -0,0 +1,23 @@
+bedroom_choices = {
+    '1':1,
+    '2':2,
+    '3':3,
+    '4':4,
+    '5':5,
+    '6':6,
+    '7':7,
+    '8':8,
+    '9':9
+}
+
+
+price_choices = {
+    '500000': '$500,000',
+    '249000': '$249,000'
+}
+
+state_choices = {
+    'Distrito Federal': 'DF',
+    'Minas Gerais': 'MG'
+}
+
